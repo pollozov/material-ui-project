319b62d2be395952e9d97a729444958269baadcf
Polina Loz <pollozov@gmail.com>
2023-01-14 14:36:02 +0100

    add photo album components

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,38 @@ import Card from "@mui/material";
 import CardActions from "@mui/material";
 import CardContent from "@mui/material";
 import CardMedia from "@mui/material";
-import CssBaseline from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
 import Grid from "@mui/material";
-import Toolbar from "@mui/material";
-import Container from "@mui/material";
+import Toolbar from '@mui/material/Toolbar';
+import Container from "@mui/material/Container";
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 
 const App = () => {
     return (
-        <div>
-            <Typography variant="h1">Hello world!</Typography>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCameraIcon />
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Hello everyone, this is a photo album
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
+            
+        </>
     );
 }
 
