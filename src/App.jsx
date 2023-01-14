f93ebcd96cd5bae73948ce19f723c7af36bec286
Polina Loz <pollozov@gmail.com>
2023-01-14 14:24:16 +0100

    imports

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+import { AppBar } from "@mui/material";
+import Card from "@mui/material";
+import CardActions from "@mui/material";
+import CardContent from "@mui/material";
+import CardMedia from "@mui/material";
+import CssBaseline from "@mui/material";
+import Grid from "@mui/material";
+import Toolbar from "@mui/material";
+import Container from "@mui/material";
+
+const App = () => {
+    return (
+        <div>
+            <Typography variant="h1">Hello world!</Typography>
+        </div>
+    );
+}
+
+export default App;
